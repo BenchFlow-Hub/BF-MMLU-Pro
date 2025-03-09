7ddc41a17a1c763dbdf63d15eaaa8e294b65a5ab
kirk <kirk@benchmarkthing.com>
2025-03-09 05:28:51 +0000

    fix: change to inteligence url

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-INTELLIGENCE_URL=${INTELLIGENCE_URL}
+INTELLIGENCE_URL=${INTELLIGENCE_URL:-${AGENT_URL}}
 TEST_START_IDX=${TEST_START_IDX:-"all"}
 
 python evaluate_from_api.py --intelligence_url $INTELLIGENCE_URL --assigned_subjects $TEST_START_IDX
